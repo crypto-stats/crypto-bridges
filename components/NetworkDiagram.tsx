c571ee8725ffaa1d7eccd29ea625dfb6d9ec8ff9
Adrien Coffre <coffre.a@gmail.com>
2022-08-12 18:27:24 +0200

    Add graph highlighting

--- components/NetworkDiagram.tsx
+++ components/NetworkDiagram.tsx
@@ -1,8 +1,7 @@
 import * as d3 from 'd3';
 import { SimulationNodeDatum } from 'd3';
 import { useRouter } from 'next/router';
-import type { RefObject } from 'react';
-import { useEffect, useRef } from 'react';
+import { RefObject, useEffect, useRef, useState } from 'react';
 import useSWR from 'swr';
 import { BRIDGED_VALUE_API_URL } from '../constants';
 import style from '../styles/NetworkDiagram.module.css';
@@ -15,6 +14,15 @@ import {
   IGraphNode,
 } from '../utils';
 
+enum COLORS {
+  DEFAULT = '#6d7381',
+  SELECTED = '#D750C8',
+}
+
+interface INetworkGraph {
+  updateSelected: (path: string) => void;
+}
+
 const fetcher = (url: string) =>
   fetch(url)
     .then((res) => res.json())
@@ -25,11 +33,12 @@ const PADDING = 40;
 
 const getTvlRadius = (d: any) => Math.sqrt((d.value * RATIO) / Math.PI) * 8;
 
-function drawChart(
+function drawGraph(
   svgRef: RefObject<SVGSVGElement>,
   data: IGraphData,
   navigateTo: (path: string) => void,
-) {
+): INetworkGraph {
+  let selected: IGraphNode | undefined;
   let width = 0,
     height = 0;
   let currentSimulation:
@@ -42,6 +51,8 @@ function drawChart(
     const newPath = `/${i.type === 'bridge' ? 'bridge' : 'chain'}/${i.name
       .split(' ')
       .join('-')}`;
+    selected = i;
+    highlight();
     navigateTo(newPath);
   }
 
@@ -60,7 +71,7 @@ function drawChart(
     .enter()
     .append('path')
     .style('cursor', 'pointer')
-    .style('stroke', '#6d7381')
+    .style('stroke', COLORS.DEFAULT)
     .style('fill', 'none')
     .style('stroke-width', (d) => Math.log((d.tvl * RATIO) / 3) * 5)
     .on('click', onLineClick);
@@ -71,7 +82,7 @@ function drawChart(
   const tvlCircles = circleGroups
     .append('circle')
     .attr('r', getTvlRadius)
-    .style('fill', '#6d7381')
+    .style('fill', COLORS.DEFAULT)
     .style('cursor', 'pointer')
     .on('click', onClick);
 
@@ -86,7 +97,7 @@ function drawChart(
     .append('tspan')
     .text((d) => d.name.split(' ')[1] ?? '')
     .attr('dy', '20')
-    .attr('dx', (d: any) => -d.name.split(' ')[1]?.length * 9);
+    .attr('dx', (d: any) => -d.name.split(' ')[1]?.length * 9 || '0');
 
   const resize = () => {
     if (currentSimulation !== undefined) {
@@ -154,19 +165,65 @@ function drawChart(
       return `M${source.x},${source.y}A${dr},${dr} 0 0,1 ${target.x},${target.y}`;
     });
   }
+
+  function findSelected(path: string): IGraphNode | undefined {
+    return path.length === 1
+      ? undefined
+      : data.nodes.find((node) => node.name === path.split('/')[2]);
+  }
+
+  const highlight = () => {
+    tvlCircles.style('fill', (d: any) => {
+      if (selected === undefined || d.name !== selected.name) {
+        return COLORS.DEFAULT;
+      } else {
+        return COLORS.SELECTED;
+      }
+    });
+    links.style('stroke', (d: any) => {
+      if (selected === undefined) {
+        return COLORS.DEFAULT;
+      } else if (
+        (selected.type === 'bridge' && selected.name === d.target.name) ||
+        (selected.type === 'blockchain' && selected.name === d.source.name)
+      ) {
+        return COLORS.SELECTED;
+      } else {
+        return COLORS.DEFAULT;
+      }
+    });
+  };
+
+  const updateSelected = (path: string) => {
+    selected = findSelected(path);
+    highlight();
+  };
+
+  updateSelected(window.location.pathname);
+
+  return { updateSelected };
 }
 
 export default function NetworkDiagram() {
   const router = useRouter();
-  const navigateTo = (path: string) =>
-    router.push(path, undefined, { scroll: false });
+  const [graph, setGraph] = useState<INetworkGraph>();
   const { data, error } = useSWR(BRIDGED_VALUE_API_URL, fetcher);
   const svg = useRef<SVGSVGElement>(null);
   useEffect(() => {
     if (data === undefined) return;
+    if (graph !== undefined) return;
+    if (svg.current !== null) {
+      svg.current.innerHTML = '';
+    }
+    const navigateTo = (path: string) =>
+      router.push(path, undefined, { scroll: false });
     const convertedData = convertDataForGraph(data as ICsApiData);
-    drawChart(svg, convertedData, navigateTo);
-  }, [svg, data]);
+    const g = drawGraph(svg, convertedData, navigateTo);
+    setGraph(g);
+    const updateUrl = (path: string) => g.updateSelected(path);
+    router.events.on('routeChangeStart', updateUrl);
+    return () => router.events.off('routeChangeStart', updateUrl);
+  }, [svg, data, router, graph]);
   if (error) return null;
   if (!data) return <p>loading</p>;
   return (
