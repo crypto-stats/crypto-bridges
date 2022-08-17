b8c472ffc1310abb242921533cd6da5a9f63795b
Adrien Coffre <coffre.a@gmail.com>
2022-08-17 18:16:55 +0200

    Attempt #2 to fix missing styles on exit framer animation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,26 @@ import Panel from '../components/Panel';
 import { GRAPH_COLORS } from '../constants';
 import '../styles/globals.css';
 
+import Router from 'next/router';
+
+const routeChange = () => {
+  // Temporary fix to avoid flash of unstyled content
+  // during route transitions. Keep an eye on this
+  // issue and remove this code when resolved:
+  // https://github.com/vercel/next.js/issues/17464
+
+  const tempFix = () => {
+    const allStyleElems = document.querySelectorAll('style[media="x"]');
+    allStyleElems.forEach((elem) => {
+      elem.removeAttribute('media');
+    });
+  };
+  tempFix();
+};
+
+Router.events.on('routeChangeComplete', routeChange);
+Router.events.on('routeChangeStart', routeChange);
+
 function MyApp({ Component, pageProps, router }: AppProps) {
   return (
     <>
