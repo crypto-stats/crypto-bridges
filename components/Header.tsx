aff8ac1b4644d23bc3b09659eaad12f388de1dfb
Adrien Coffre <coffre.a@gmail.com>
2022-08-29 23:59:30 +0800

    Update header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { HEADER_HEIGHT, PANEL_WIDTH } from '../constants';
 import styles from '../styles/Header.module.css';
 import { needsLandscape } from '../utils';
 
 export default function Header() {
+  const [isHorizontal, setHorizontal] = useState(false);
   const el = useRef<HTMLDivElement>(null);
   if (el.current !== null) {
     el.current.style.height = `${HEADER_HEIGHT}px`;
@@ -13,6 +14,7 @@ export default function Header() {
   useEffect(() => {
     const updateToSidePanel = () => {
       const isLandscape = needsLandscape();
+      setHorizontal(isLandscape);
       if (el.current !== null) {
         el.current.style.width = `calc(100% - ${
           isLandscape ? PANEL_WIDTH : 0
@@ -22,37 +24,43 @@ export default function Header() {
     updateToSidePanel();
     window.addEventListener('resize', updateToSidePanel);
     return () => window.removeEventListener('resize', updateToSidePanel);
-  }, []);
+  }, [setHorizontal]);
   return (
     <header className={styles.header} ref={el}>
-      <Link href={'/'} passHref={true} scroll={false}>
-        <a>
-          <span className={styles.verticalAlign}>
-            <Image src="/logo.png" width={45} height={45} alt="" />
-          </span>
-          <h1>Cryptobridges.info</h1>
-        </a>
-      </Link>
-      <p className={styles.baseline}>
-        <span className={styles.poweredBy}>Powered by</span>{' '}
-        <a
-          href="https://cryptostats.community"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <b className={styles.verticalAlign}>
+      <div className={isHorizontal ? styles.onelineHorizontal : styles.oneline}>
+        <Link href={'/'} passHref={true} scroll={false}>
+          <a>
+            {/* If logo needed, code below is ready, needs update the h1 margins.
             <span className={styles.verticalAlign}>
-              <Image
-                src={'/cryptostats.svg'}
-                alt="logo"
-                width={20}
-                height={20}
-                className={styles.verticalAlign}
-              />
-            </span>{' '}
-            CryptoStats
-          </b>
-        </a>
+              <Image src="/logo.png" width={45} height={45} alt="" />
+            </span> */}
+            <h1>Cryptoflows.info</h1>
+          </a>
+        </Link>
+        <p className={styles.baseline}>
+          <span className={styles.poweredBy}>Powered by</span>{' '}
+          <a
+            href="https://cryptostats.community"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <b className={styles.verticalAlign}>
+              <span className={styles.verticalAlign}>
+                <Image
+                  src={'/cryptostats.svg'}
+                  alt="logo"
+                  width={20}
+                  height={20}
+                  className={styles.verticalAlign}
+                />
+              </span>{' '}
+              CryptoStats
+            </b>
+          </a>
+        </p>
+      </div>
+      <p className={isHorizontal ? styles.questionHorizontal : styles.question}>
+        Where does value flow in crypto?
       </p>
     </header>
   );
