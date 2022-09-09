6a4f960d854c7499841c58f7bc1327c8b3b4e1f3
Adrien Coffre <coffre.a@gmail.com>
2022-09-09 17:13:40 +0800

    Limit displayed chains

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import type { NextPage } from 'next';
+import { useEffect, useState } from 'react';
 import Motion from '../components/Motion';
 import Table from '../components/Table';
 import { loadData } from '../data/load-data';
 import { GetStaticBridgeProps, IDummyData } from '../data/types';
 import styles from '../styles/index.module.css';
-import { convertDummyDataForGraph } from '../utils';
+import { convertDummyDataForGraph, needsLandscape } from '../utils';
+
+const DEFAULT_MAX_ELEMENTS = 4;
 
 interface HomePageProps {
   data: IDummyData;
@@ -12,36 +15,71 @@ interface HomePageProps {
 
 const Home: NextPage<HomePageProps> = ({ data }) => {
   const convertedData = convertDummyDataForGraph(data);
+  const [displayLimit, setDisplayLimit] = useState(DEFAULT_MAX_ELEMENTS);
+  const [showAllExports, setShowAllExports] = useState(false);
+  const [showAllImports, setShowAllImports] = useState(false);
+  const showExports = () => setShowAllExports(true);
+  const showImports = () => setShowAllImports(true);
+  const exports = convertedData.nodes.map((node) => ({
+    id: node.id,
+    name: node.name,
+    logo: node.logo,
+    tvl: node.tvl,
+    in: node.in,
+    bridgedIn: 0,
+    bridgedOut: 0,
+  }));
+  const imports = convertedData.nodes.map((node) => ({
+    id: node.id,
+    name: node.name,
+    logo: node.logo,
+    tvl: node.tvl,
+    in: node.in,
+    bridgedIn: 0,
+    bridgedOut: 0,
+  }));
+  useEffect(() => {
+    const findMaxElements = () => {
+      const isLandscape = needsLandscape();
+      if (!isLandscape) {
+        return DEFAULT_MAX_ELEMENTS;
+      }
+      const freeSpace = (innerHeight - 40 - 40 * 3) / 2 - 24 - 40 - 16 - 51;
+      const maxElements = Math.floor(freeSpace / 60);
+      setDisplayLimit(maxElements);
+      return maxElements;
+    };
+    window.addEventListener('resize', findMaxElements);
+    return () => window.removeEventListener('resize', findMaxElements);
+  }, [setDisplayLimit]);
   return (
     <Motion>
       <menu className={styles.menu}>
         <Table
           listsChains={true}
           title="Top Exporters"
-          tableContent={convertedData.nodes.map((node) => ({
-            id: node.id,
-            name: node.name,
-            logo: node.logo,
-            tvl: node.tvl,
-            in: node.in,
-            bridgedIn: 0,
-            bridgedOut: 0,
-          }))}
+          tableContent={
+            showAllExports ? exports : exports.slice(0, displayLimit)
+          }
         />
+        {!showAllExports && (
+          <button onClick={showExports} className={styles.seeAll}>
+            See all chains
+          </button>
+        )}
         <Table
           listsChains={true}
           title="Top Importers"
           valueIn
-          tableContent={convertedData.nodes.map((node) => ({
-            id: node.id,
-            name: node.name,
-            logo: node.logo,
-            tvl: node.tvl,
-            in: node.in,
-            bridgedIn: 0,
-            bridgedOut: 0,
-          }))}
+          tableContent={
+            showAllImports ? imports : imports.slice(0, displayLimit)
+          }
         />
+        {!showAllImports && (
+          <button onClick={showImports} className={styles.seeAll}>
+            See all chains
+          </button>
+        )}
       </menu>
     </Motion>
   );
