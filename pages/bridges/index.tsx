46d7a03bffaa82cbf01cb3c0215edc514eb8f414
Adrien Coffre <coffre.a@gmail.com>
2022-09-13 06:50:27 +0800

    Fix bridge index needs consume data

--- pages/bridges/index.tsx
+++ pages/bridges/index.tsx
@@ -1,13 +1,25 @@
 import type { NextPage } from 'next';
 import Motion from '../../components/Motion';
+import { loadData } from '../../data/load-data';
+import { IDummyData } from '../../data/types';
 import styles from '../../styles/page.module.css';
 
-const Bridges: NextPage = () => {
+interface IBridgeProps {
+  data: IDummyData;
+}
+
+const Bridges: NextPage<IBridgeProps> = ({ data }) => {
+  data.bridges[0];
   return (
     <Motion>
       <section className={styles.section}>Todo: Bridges page</section>
     </Motion>
   );
+};
+
+export const getStaticProps = async () => {
+  const data = await loadData();
+  return { props: { data }, revalidate: 5 * 60 };
 };
 
 export default Bridges;
